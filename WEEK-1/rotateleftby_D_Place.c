190cc59016811b9f59724543c427e28252a7800c
AdeshGupta3135 <141109428+AdeshGupta3135@users.noreply.github.com>
2023-08-01 10:45:24 +0530

    Create rotateleftby_D_Place.c

---
+++ WEEK-1/rotateleftby_D_Place.c
@@ -0,0 +1,32 @@
+void rotate(int arr[], int n, int k) {
+    if (k == 0) {
+        return;
+    }
+
+
+    int temp = arr[n-1];
+int i;
+    for (i = n-1; i>0; i--) {
+        arr[i] = arr[i-1];
+    }
+    arr[0] = temp;
+
+
+    rotate(arr, n, k-1);
+}
+
+
+int main() {
+    int arr[] = {1, 3, 5, 7, 9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int k = 1;
+
+    rotate(arr, n, k);
+    int j;
+    for ( j= 0; j < n; j++) {
+        printf("%d ", arr[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
